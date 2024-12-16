5b73b471455963a8ef0669057a485c3ecc4183a8
v8zolotarev <vadim8zolotarev@gmail.com>
2024-12-16 13:33:33 +0300

    Initial commit

---
+++ src/test/java/org/zolotarev/t1schoolstarter/T1SchoolStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zolotarev.t1schoolstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1SchoolStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

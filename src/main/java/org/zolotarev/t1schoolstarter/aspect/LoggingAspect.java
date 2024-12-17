f62d32f5527d0492e8c957e48cc1283a15d45cea
v8zolotarev <vadim8zolotarev@gmail.com>
2024-12-17 19:59:08 +0300

    Fixed logging starter

--- src/main/java/org/zolotarev/t1schoolstarter/aspect/LoggingAspect.java
+++ src/main/java/org/zolotarev/t1schoolstarter/aspect/LoggingAspect.java
@@ -20,39 +20,64 @@ public class LoggingAspect {
 
     private boolean isLoggable(LoggingLevel level) {
         return loggerProperties.isEnabled() &&
-                level.ordinal() >= loggerProperties.getLevel().ordinal();
+                level.ordinal() >= loggerProperties.getLevel().ordinal();  // Сравниваем уровни
+    }
+
+    private void logMessage(LoggingLevel logLevel, String message, Object... args) {
+        if (isLoggable(logLevel)) {
+            switch (logLevel) {
+                case DEBUG:
+                    log.debug(message, args);
+                    break;
+                case INFO:
+                    log.info(message, args);
+                    break;
+                case WARNING:
+                    log.warn(message, args);
+                    break;
+                case ERROR:
+                    log.error(message, args);
+                    break;
+                case CRITICAL:
+                    log.error(message, args);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     @Before("@annotation(org.zolotarev.t1schoolstarter.aspect.annotations.LogBefore)")
     public void logBefore(JoinPoint joinPoint) {
-        log.info("Method {} executed", joinPoint.getSignature().getName());
+        logMessage(loggerProperties.getLevel(), "Method {} executed", joinPoint.getSignature().getName());
     }
 
     @AfterThrowing(value = "@annotation(org.zolotarev.t1schoolstarter.aspect.annotations.LogAfterThrowing)", throwing = "exception")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable exception) {
-        log.error("Method {} threw an exception: {}",
+        logMessage(loggerProperties.getLevel(), "Method {} threw an exception: {}",
                 joinPoint.getSignature().getName(), exception.getMessage());
     }
 
     @AfterReturning(value = "@annotation(org.zolotarev.t1schoolstarter.aspect.annotations.LogAfterReturning)", returning = "result")
     public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        log.info("Method {} executed successfully. Result: {}", joinPoint.getSignature().getName(), result);
+        logMessage(loggerProperties.getLevel(), "Method {} executed successfully. Result: {}",
+                joinPoint.getSignature().getName(), result);
     }
 
     @Around("@annotation(org.zolotarev.t1schoolstarter.aspect.annotations.LogAround)")
     public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
-        log.info("Execution of method '{}' started. Parameters: {}",
+        logMessage(loggerProperties.getLevel(), "Execution of method '{}' started. Parameters: {}",
                 joinPoint.getSignature().getName(),
                 joinPoint.getArgs());
 
         Object result;
         try {
             result = joinPoint.proceed();
-            log.info("Execution of method '{}' completed successfully. Result: {}",
+            logMessage(loggerProperties.getLevel(), "Execution of method '{}' completed successfully. Result: {}",
                     joinPoint.getSignature().getName(),
                     result);
         } catch (Throwable ex) {
-            log.error("Execution of method '{}' failed. Exception: {} - {}",
+            logMessage(loggerProperties.getLevel(), "Execution of method '{}' failed. Exception: {} - {}",
                     joinPoint.getSignature().getName(),
                     ex.getClass().getSimpleName(),
                     ex.getMessage());
